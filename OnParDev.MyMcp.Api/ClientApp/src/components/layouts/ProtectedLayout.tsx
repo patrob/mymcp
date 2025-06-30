f0542a3f3ef2a4dc6789090ad431bbc3f58078ce
Patrick Robinson <patrob@gmail.com>
2025-06-30 08:09:37 -0500

    try to fix some warnings

--- OnParDev.MyMcp.Api/ClientApp/src/components/layouts/ProtectedLayout.tsx
+++ OnParDev.MyMcp.Api/ClientApp/src/components/layouts/ProtectedLayout.tsx
@@ -1,6 +1,6 @@
 import { ReactNode } from 'react'
 import { ClerkProvider, SignedIn, SignedOut, RedirectToSignIn } from '@clerk/clerk-react'
-import { useConfiguration } from '@/contexts/ConfigurationContext'
+import { useConfiguration } from '@/hooks/useConfiguration'
 
 interface ProtectedLayoutProps {
   children: ReactNode
